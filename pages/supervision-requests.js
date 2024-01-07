99bb9a9d39ff002668279da025b5eea38717b24c
themaleem <zizo9585@gmail.com>
2024-01-07 17:17:08 +0100

    fix

--- pages/supervision-requests.js
+++ pages/supervision-requests.js
@@ -1,19 +1,68 @@
 import PropTypes from "prop-types";
+import { useCallback, useEffect, useState } from "react";
 
+import CustomTabs from "../components/customTabs";
 import authWrapper from "../containers/hoc/authWrapper";
 import DashboardLayout from "../components/layout/dashboard";
-import SupervisionRequestList from "../containers/superadmin/supervisionRequest";
+import SupervisorRequestsList from "../containers/supervisionRequest";
 
-const SupervisionRequestPage = ({ auth }) => {
+const path = "supervisionRequestsPath";
+
+const tabsObject = {
+  pending: 0,
+  approved: 1,
+  rejected: 2,
+};
+
+const SupervisionRequestsPage = ({ auth }) => {
+  const getTabs = useCallback(() => {
+    return [
+      {
+        tabIndex: 0,
+        disabled: false,
+        title: "Pending",
+        panel: (
+          <SupervisorRequestsList auth={auth} status="pending" path={path} />
+        ),
+      },
+      {
+        tabIndex: 1,
+        disabled: false,
+        title: "Approved",
+        panel: (
+          <SupervisorRequestsList auth={auth} status="approved" path={path} />
+        ),
+      },
+      {
+        tabIndex: 2,
+        disabled: false,
+        title: "Rejected",
+        panel: (
+          <SupervisorRequestsList auth={auth} status="rejected" path={path} />
+        ),
+      },
+    ];
+  }, [auth]);
+
+  const [tabs, setTabs] = useState(() => getTabs());
+
+  useEffect(() => {
+    setTabs(getTabs);
+  }, [getTabs]);
+
   return (
     <DashboardLayout>
-      <SupervisionRequestList auth={auth} />
+      <CustomTabs
+        tabs={tabs}
+        tabsObject={tabsObject}
+        pathName="supervisorSupervisionRequestsPath"
+      />
     </DashboardLayout>
   );
 };
 
-SupervisionRequestPage.propTypes = {
+SupervisionRequestsPage.propTypes = {
   auth: PropTypes.instanceOf(Object).isRequired,
 };
 
-export default authWrapper(SupervisionRequestPage);
+export default authWrapper(SupervisionRequestsPage);
